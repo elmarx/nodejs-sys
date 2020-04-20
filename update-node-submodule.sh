c51f73da928d6a774e81684f9c2eb412e45d9555
Elmar Athmer <elmar@athmer.org>
2020-04-20 19:42:03 +0200

    add helper to update nodejs-sources/napi-version if necessary

---
+++ update-node-submodule.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+cd node
+git fetch
+MOST_RECENT_VERSION=$(git tag | sort -V | tail -n 1 )
+CURRENT_VERSION=$(git describe --tags HEAD)
+
+if [ "$MOST_RECENT_VERSION" = "$CURRENT_VERSION" ]; then
+  echo "$CURRENT_VERSION is up to date, nothing to do"
+  exit 0
+fi
+
+CHANGED_FILES=$(git diff --name-only "$CURRENT_VERSION".."$MOST_RECENT_VERSION" src/node_api.h src/js_native_api.h src/js_native_api_types.h src/node_api_types.h)
+
+if [ -z "$CHANGED_FILES" ]; then
+  echo "Relevant files did not change between $CURRENT_VERSION..$MOST_RECENT_VERSION"
+  exit 0
+else
+  echo "changed files:"
+  echo "$CHANGED_FILES"
+fi
+
+git checkout "$MOST_RECENT_VERSION"
+
+if ! grep "NAPI_VERSION  6" src/node_version.h; then
+  echo "Detected new NAPI_VERSION. Refusing to auto-update. Please add feature-flag etc."
+  exit 1
+fi
+
+# back to src-root
+cd ..
+git commit -m "update node $CURRENT_VERSION -> $MOST_RECENT_VERSION" node
