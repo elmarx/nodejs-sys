11730f731b8b3a3147a277d31a1dc9f18a733a93
NickNaso <nicoladelgobbo@gmail.com>
2020-09-23 22:19:02 +0200

    :pencil: update documentation

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ Features
 Different API versions may be selected via feature-flag. See the [N-API Version Matrix](https://nodejs.org/dist/latest-v14.x/docs/api/n-api.html#n_api_n_api_version_matrix) for details.
 
 - `napi_v5` supported by all [actively maintained](https://nodejs.org/en/about/releases/) NodeJS releases
-- `napi_v6` requires at least `v13.7.0`, this is the **default** (if no flag is given)
+- `napi_v6` supported by all [actively maintained](https://nodejs.org/en/about/releases/) NodeJS releases
+- `napi_v7` requires at least `v14.12.0`, this is the **default** (if no flag is given)
 - the experimental N-API features may be enabled via feature flag `experimental` (*off* by default)
 
 Updates
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Low level bindings to nodejs' [N-API](https://nodejs.org/dist/latest-v13.x/docs/api/n-api.html)
+//! Low level bindings to nodejs' [N-API](https://nodejs.org/dist/latest-v14.x/docs/api/n-api.html)
 //!
 //!
 //! # Features
@@ -6,7 +6,8 @@
 //! Different API versions may be selected via feature-flag. See the [N-API Version Matrix](https://nodejs.org/dist/latest-v14.x/docs/api/n-api.html#n_api_n_api_version_matrix) for details.
 //!
 //! - `napi_v5` supported by all [actively maintained](https://nodejs.org/en/about/releases/) NodeJS releases
-//! - `napi_v6` requires at least `v13.7.0`, this is the **default** (if no flag is given)
+//! - `napi_v6` supported by all [actively maintained](https://nodejs.org/en/about/releases/) NodeJS releases
+//! - `napi_v7` requires at least `v14.12.0`, this is the **default** (if no flag is given)
 //! - the experimental N-API features may be enabled via feature flag `experimental` (*off* by default)
 
 #![allow(non_upper_case_globals)]
